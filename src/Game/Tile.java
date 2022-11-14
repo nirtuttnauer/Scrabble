bfc4a92c8c9278ebbbdfdab8aad9c637f4a02cbb
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2022-11-14 10:26:38 +0200

    Initial commit

---
+++ src/Game/Tile.java
@@ -0,0 +1,20 @@
+package Game;
+
+public class Tile {
+    private char  _letter;
+
+    public char get_letter() {
+        return _letter;
+    }
+
+    public void set_letter(char _letter) {
+        this._letter = _letter;
+    }
+
+    public class Bag{
+    Tile Tiles[];
+    Bag(){
+
+    }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import Game.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
